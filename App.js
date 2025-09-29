880ce3c9bae12b9df65f6c641d0d28337432ecb2
2k23csbs46-stack <2k23csbs46@kiot.ac.in>
2025-09-29 11:26:17 +0530

    App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import HelloWorld from "./components/HelloWorld";
+import Header from "./components/Header";
+import FirstComp from "./components/FirstComp";
+
+function App() {
+  return <>
+<Header/>
+
+   </>
+}
+
+export default App;
